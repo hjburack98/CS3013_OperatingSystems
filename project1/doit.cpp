2d58ea9c3876a93ee6aa79a749c986eb3d65045a
Hjburack98 <hjburack@gmail.com>
2019-08-27 12:31:26 -0400

    lets see this time

--- project1/doit.cpp
+++ project1/doit.cpp
@@ -29,7 +29,7 @@ void printStats(long startTime){ //start time in ms
     userEnd = usage.ru_utime;
     systemEnd = usage.ru_stime;
     double userEndTime = ((userEnd.tv_usec / 1000));
-    double systemEndTime = ((systemEnd.tv_usec));// / 1000));
+    double systemEndTime = ((systemEnd.tv_usec / 1000));
     double wallClockTime = endTime - startTime;
     cout << "System Statistics For Process:\n";
     cout << "     User CPU Time: " << userEndTime << " milliseconds\n";
