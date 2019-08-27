4011925f7ebacc9d9e4578c5eb8405752f560952
Hjburack98 <hjburack@gmail.com>
2019-08-27 12:02:42 -0400

    changed timing issue to read out milliseconds

--- project1/doit.cpp
+++ project1/doit.cpp
@@ -24,12 +24,12 @@ void printStats(long startTime){ //start time in ms
     struct rusage usage;
     struct timeval processEnd, userEnd, systemEnd;
     gettimeofday(&processEnd, NULL);
-    long endTime = ((processEnd.tv_sec * 1000) + (processEnd.tv_sec / 1000));
+    long endTime = ((processEnd.tv_sec * 1000));// + (processEnd.tv_sec / 1000));
     getrusage(RUSAGE_CHILDREN, &usage);
     userEnd = usage.ru_utime;
     systemEnd = usage.ru_stime;
-    double userEndTime = ((userEnd.tv_sec * 1000) + (userEnd.tv_sec / 1000));
-    double systemEndTime = ((systemEnd.tv_sec * 1000) + (systemEnd.tv_sec / 1000));
+    double userEndTime = ((userEnd.tv_sec * 1000));// + (userEnd.tv_sec / 1000));
+    double systemEndTime = ((systemEnd.tv_sec * 1000));// + (systemEnd.tv_sec / 1000));
     double wallClockTime = endTime - startTime;
     cout << "System Statistics For Process:\n";
     cout << "     User CPU Time: " << userEndTime << " milliseconds\n";
@@ -49,7 +49,7 @@ int run (char ** inputArgs)
     struct timeval processStart;
 
     gettimeofday(&processStart, NULL);
-    startTime = ((processStart.tv_sec * 1000) + (processStart.tv_sec / 1000));
+    startTime = ((processStart.tv_sec * 1000));// + (processStart.tv_sec / 1000));
 
     //fork process
     pid = fork();
