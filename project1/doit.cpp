e627d874a4865c61c239f909676735cf313b0936
Hjburack98 <hjburack@gmail.com>
2019-08-28 17:10:59 -0400

    completed assignment

--- project1/doit.cpp
+++ project1/doit.cpp
@@ -2,229 +2,125 @@
 #include <iostream>
 using namespace std;
 #include <sys/types.h>
-#include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <vector>
 #include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+#include <vector>
 #include <stdlib.h>
-#define MAX_ARGS 32
 #define MAX_CHARS 128
-
+#define MAX_ARGS 32
 
 typedef struct {
-    int pid;
-    string cmd;
-    long start;
-} process; 
+	int pid;
+	string cmd;
+	long start;
+} process;
 
-char *prompt = (char *)malloc(16 * sizeof(char));
+char *prompt = (char *)malloc(32 * sizeof(char));
 int ampersand = 0; //if process in running in background
 vector<process> children; //vector to dynamically allocade
 
-void printStats(long startTime){ //start time in ms
-    struct rusage usage;
-    struct timeval processEnd, userEnd, systemEnd;
 
-    //set the end time
-    gettimeofday(&processEnd, NULL);
-    long endTime = ((processEnd.tv_usec / 1000));
+void printStats(long startTime) { //start time in ms
+	struct rusage usage;
+	struct timeval processEnd, userEnd, systemEnd;
 
-    //get all data for stats
-    getrusage(RUSAGE_CHILDREN, &usage);
-    userEnd = usage.ru_utime;
-    systemEnd = usage.ru_stime;
-    double userEndTime = ((userEnd.tv_usec / 1000));
-    double systemEndTime = ((systemEnd.tv_usec / 1000));
-    double wallClockTime = endTime - startTime;
+    //set end time
+	gettimeofday(&processEnd, NULL);
+	long endTime = (processEnd.tv_sec * 1000) + (processEnd.tv_usec / 1000);
 
-    //return stats
-    cout << "System Statistics For Process:\n";
-    cout << "     User CPU Time: " << userEndTime << " milliseconds\n";
-    cout << "     System CPU Time: " << systemEndTime << " milliseconds\n";
-    cout << "     Wall Clock Time: " << wallClockTime << " milliseconds\n";
-    cout << "     Involuntary Context Switches: " << usage.ru_nivcsw << endl;
-    cout << "     Voluntary Context Switches: " << usage.ru_nvcsw << endl;
-    cout << "     Major Page Faults: " << usage.ru_majflt << endl;
-    cout << "     Minor Page Faults: " << usage.ru_minflt << endl;
+    //get all the data for stats
+	getrusage(RUSAGE_CHILDREN, &usage);
+	userEnd = usage.ru_utime;
+	systemEnd = usage.ru_stime;
+
+    //convert times to ms
+	double userTime = ((userEnd.tv_sec * 1000) + (userEnd.tv_usec / 1000));
+	double systemTime = ((systemEnd.tv_sec * 1000) + (systemEnd.tv_usec / 1000));
+	double wallClockTime = endTime - startTime;
+	cout << "USAGE STATS:\n";
+	cout << "   system time = " << systemTime << " ms\n";
+	cout << "   user time = " << userTime << " ms\n";
+	cout << "   wall clock time = " << wallClockTime << " ms\n";
+	cout << "   involuntary context switches = " << usage.ru_nivcsw << endl;
+	cout << "   voluntary context switches = " << usage.ru_nvcsw << endl;
+	cout << "   page faults requiring I/O = " << usage.ru_majflt << endl;
+	cout << "   page faults serviced without I/O = " << usage.ru_minflt << endl;
 }
 
+void safeExit() {
+	if (children.size() > 0) { //if the child is still running
+		cout << "Waiting for " << children.size() << " process(es) to finish" << endl;
+		for (unsigned long i = 0; i < children.size(); i++) {
+			int status;
+			pid_t result = waitpid(children.at(i).pid, &status, 0);
 
-int run (char ** inputArgs)
-{
-    int pid;
-    long startTime;
-    struct timeval processStart;
-
-    //set start time
-    gettimeofday(&processStart, NULL);
-    startTime = ((processStart.tv_usec / 1000));
-
-    //fork process
-    pid = fork();
-
-    if(pid < 0){ //fork error
-        cerr << "FORK ERROR\n";
-        exit(1);
-    }
-    else if(pid == 0) { //child process
-        if(execvp(inputArgs[0], inputArgs) < 0){ //execution error
-            cerr << "EXECUTION ERROR\n";
-            exit(1);
-        }
-        return 0;
-    }
-    else { //parent process
-        if(!ampersand) { //if there are no background processes
-            wait(0);
-            printStats(startTime);
-            return 0;
-        }
-
-        else { //with a background process
-            process child = {pid, inputArgs[0], startTime};
-            children.push_back(child);
-            cout << "[" << children.size() << "] " << children.back().pid << endl;
-            return 0;
-        }
-    }
-
+			if (result > 0) { //if the child finished
+				cout << "[" << i + 1 << "] " << children.at(i).pid << " Completed\n";
+				printStats(children.at(i).start);
+			}
+		}
+	}
+	exit(0);
 }
 
-void exit() {
-    if(children.size() > 0) { //need child to finish running
-        cout << children.size() << "still need to finish" << endl;
-        
-        for(unsigned long i = 0; i < children.size(); i++){
-            int status;
-            pid_t result = waitpid(children.at(i).pid, &status, 0);
-            if(result > 0){ //once child has finished running
-                cout << "[" << i+1 << "] " << children.at(i).pid << " completed\n";
-                printStats(children.at(i).start);
-            }
-        }
-    }
-    exit(0);
+int run(char ** newArgs) {
+	int pid;
+	long start_ms;
+	struct timeval wc_start;
+
+	gettimeofday(&wc_start, NULL);
+	start_ms = ((wc_start.tv_sec * 1000) + (wc_start.tv_usec / 1000));
+
+	/* fork it */
+	pid = fork();
+		
+	if (pid < 0) { //fork error
+		cerr << "FORK ERROR\n";
+		exit(1);
+	} 
+    else if (pid == 0) { //child running
+		if (execvp(newArgs[0], newArgs) < 0) { //execution error
+			cerr << "EXECUTION ERROR\n";
+			exit(1);
+		}
+		return 0;
+	} 
+    else { //parent running
+		if (!ampersand) { //if no background processes
+			wait(0);
+			printStats(start_ms);
+			return 0;
+		} 
+        else {
+			process child = {pid, newArgs[0], start_ms};
+			children.push_back(child);
+			cout << "[" << children.size() << "] " << children.back().pid << endl;
+			return 0;
+		}
+	}
 }
 
-int runLine(char **out){
-    char line[MAX_CHARS];
-    char *token;
-    int position = 0;
+int main(int argc, char *argv[]) {
+	char **newArgs = (char **)malloc(MAX_ARGS * sizeof(char *));
+	int i;
+	prompt[0] = '=';
+	prompt[1] = '=';
+	prompt[2] = '>';
+	prompt[3] = '\0';
 
-    cin.getline(line, MAX_CHARS);
-    token = strtok(line, " ");
-    while(token != NULL) {
-        out[position] = token;
-        token = strtok(NULL, " ");
-        position++;
-    }
+	if (argc > 1) { //Command Line
+		for (i = 1; i < argc; i++) {
+			newArgs[i - 1] = argv[i];
+		}
+		newArgs[argc - 1] = NULL;
+		run(newArgs);
+	} 
 
-    if(strcmp(out[position - 1], "&") == 0){ //if there is a backround process
-        ampersand = 1;
-        out[position - 1] = NULL;
-    }
-    else {
-        ampersand = 0;
-        out[position - 1] = NULL;
-    }
-
-    if(strcmp(out[0], "exit") == 0) { //exit
-        exit();
-        return 1;
-    }
-    else if(strcmp(out[0], "cd") == 0 && out[1] != NULL){ //cd
-        if(chdir(out[1]) != 0){
-            cerr << "CHDIR ERROR\n";
-        }
-    }   
-    else if(strcmp(out[0], "set") == 0 && strcmp(out[1], "prompt") == 0 && strcmp(out[2], "=") == 0 && out[3] != NULL) { //set prompt
-        strcpy(prompt, out[3]);
-    }
-    else {
-        run(out);
-    }
-
-    return 0;
-}
-
-int main(int argc, char *argv[]){
-    //allocate required size
-    char **newArgs = (char **)malloc(MAX_ARGS * sizeof(char *));
-    int i;
-
-    prompt[0] = '=';
-    prompt[1] = '=';
-    prompt[2] = '>';
-    prompt[3] = '\0';
-
-    if(argc > 1){ //if there's greater than one argument, code takes the command line argument
-        for(i = 1; i < argc; i++){
-        newArgs[i - 1] = argv[i];
-        }
-
-        newArgs[argc - 1] = NULL;
-        run(newArgs);
-    }
-    /*else { //if there is only one argument, enter shell mode
-        while(1){
-            cout << prompt << " ";
-
-            char line[MAX_CHARS];
-            char *token;
-            int position = 0;
-
-            cin.getline(line, MAX_CHARS); //read line
-
-            for(unsigned long j = 0; j < children.size(); j++){ //background processes
-                int childStatus;
-                pid_t result = waitpid(children.at(j).pid, &childStatus, WNOHANG);
-
-                if(result > 0){ //once completed
-                    cout << "[" << j + 1 << "] " << children.at(j).pid << " completed\n";
-                    printStats(children.at(j).start);
-                    children.erase(children.begin() + j);
-                }
-            }
-
-            //make input a token
-            token = strtok(line, " ");
-            while(token != NULL){
-                newArgs[position] = token;
-                token = strtok(NULL, " ");
-                position++;
-            }
-
-
-            if(strcmp(newArgs[position - 1], "&") == 0){ //if there is a backround process
-                ampersand = 1;
-                newArgs[position - 1] = NULL;
-            }
-            else {
-                ampersand = 0;
-                newArgs[position - 1] = NULL;
-            }
-
-            if(strcmp(newArgs[0], "exit") == 0) { //exit
-                exit();
-                return 1;
-            }
-            else if(strcmp(newArgs[0], "cd") == 0 && newArgs[1] != NULL){ //cd
-                if(chdir(newArgs[1]) != 0){
-                    cerr << "CHDIR ERROR\n";
-                }
-            }   
-            else if(strcmp(newArgs[0], "set") == 0 && strcmp(newArgs[1], "prompt") == 0 && strcmp(newArgs[2], "=") == 0 && newArgs[3] != NULL) { //set prompt
-                strcpy(prompt, newArgs[3]);
-            }
-            else {
-                run(newArgs);
-            }
-        }
-    } */
-    else { //if there is only one argument, enter shell mode
-		while(1) {
+    else { //Shell
+		while (1) {
 			// print prompt
 			cout << prompt << " ";
 
@@ -232,12 +128,13 @@ int main(int argc, char *argv[]){
 			char *token;
 			int position = 0;
 
-			cin.getline(line, MAX_CHARS); // read in line
+			cin.getline(line, MAX_CHARS); //read shell line
 
-			// check for finishing background processes
+			//check to see if background processes finished
 			for (unsigned long j = 0; j < children.size(); j++) {
 				int childStatus;
 				pid_t result = waitpid(children.at(j).pid, &childStatus, WNOHANG);
+
 				if (result > 0) { // child quit
 					cout << "[" << j + 1 << "] " << children.at(j).pid << " Completed\n";
 					printStats(children.at(j).start);
@@ -253,35 +150,42 @@ int main(int argc, char *argv[]){
 				position++;
 			}
 
-			if (strcmp(newArgs[position - 1], "&") == 0) { // background process
+            //Check for background process
+			if (strcmp(newArgs[position - 1], "&") == 0) { 
 				ampersand = 1;
 				newArgs[position - 1] = NULL;
-			} else {
+			} 
+            else {
 				ampersand = 0;
 				newArgs[position] = NULL;
 			}
-			if (strcmp(newArgs[0], "exit") == 0) { // exit command
-				exit();
-			} else if (strcmp(newArgs[0], "cd") == 0 && newArgs[1] != NULL)  { // cd command
-				if (chdir(newArgs[1]) != 0)
+			if (strcmp(newArgs[0], "exit") == 0) { //exit
+				safeExit();
+
+			} 
+            else if (strcmp(newArgs[0], "cd") == 0 && newArgs[1] != NULL)  { //cd
+			    if (chdir(newArgs[1]) != 0){
 					cerr << "chdir error\n";
-			} else if (strcmp(newArgs[0], "set") == 0 && strcmp(newArgs[1], "prompt") == 0 && strcmp(newArgs[2], "=") == 0 && newArgs[3] != NULL) { // set prompt command
+                }
+
+			} 
+            else if (strcmp(newArgs[0], "set") == 0 && strcmp(newArgs[1], "prompt") == 0 && strcmp(newArgs[2], "=") == 0 && newArgs[3] != NULL) { //set prompt
 				strcpy(prompt, newArgs[3]);
-			} else if (strcmp(newArgs[0], "jobs") == 0) {
+			} 
+            else if (strcmp(newArgs[0], "jobs") == 0) {
 				if (children.size() == 0) {
 					cout << "No jobs running\n";
-				} else {
+				} 
+                else 
+                {
 					for (unsigned long i = 0; i < children.size(); i++) {
 						cout << "[" << i + 1 << "] " << children[i].pid << " " << children[i].cmd << endl;
-			}
+			    }
 				}
-			} else {
+			} 
+            else {
 				run(newArgs);
 			}
-        }
-    }
-   
+		}
+	}
 }
-
-
-
